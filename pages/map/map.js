a64c7dd3d3866852805c2ea72ab6d72df00c0aa3
itc-23006 <n23006@std.it-college.ac.jp>
2024-02-28 11:23:47 +0000

    google_map_api
    データの修正。

--- pages/map/map.js
+++ pages/map/map.js
@@ -2,6 +2,12 @@
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 const Map = ({ location }) => {
+  // プロパティが存在するか確認
+  if (!location || !location.lat || !location.lng) {
+    // エラー処理やデフォルトの座標を設定するなど、適切な対応を行う
+    return <div>Error: Location data is missing.</div>
+  }
+
   const mapContainerStyle = {
     width: '100%',
     height: '400px'
