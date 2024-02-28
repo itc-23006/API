4421d506cf4f499d8982294346a21f1dadc016c8
itc-23006 <n23006@std.it-college.ac.jp>
2024-02-28 06:03:18 +0000

    google_map_api
    コードの修正。

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,12 +1,7 @@
 // pages/map/index.js
 import Head from 'next/head'
-import { getRandomCoordinates } from '../../utils'
 
 const MapPage = () => {
-  const randomRegion = getRandomRegion() // どれか一つの地方をランダムに選択
-  const randomPrefecture = getRandomPrefectureInRegion(randomRegion) // 選択された地方から都道府県をランダムに選択
-  const randomPrefectureLocation = prefectureCoordinates[randomPrefecture]
-
   return (
     <div>
       <Head>
@@ -21,26 +16,12 @@ const MapPage = () => {
           height='450'
           frameBorder='0'
           style={{ border: 0 }}
-          src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${randomPrefectureLocation.lat},${randomPrefectureLocation.lng}`}
+          src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=Tokyo+Station`}
           allowFullScreen
         ></iframe>
       </main>
     </div>
   )
-}
-
-// ランダムに地方を選択する関数
-const getRandomRegion = () => {
-  const regions = Object.keys(prefecturesByRegion)
-  return regions[Math.floor(Math.random() * regions.length)]
-}
-
-// 選択された地方からランダムに都道府県を選択する関数
-const getRandomPrefectureInRegion = region => {
-  const prefecturesInRegion = prefecturesByRegion[region]
-  return prefecturesInRegion[
-    Math.floor(Math.random() * prefecturesInRegion.length)
-  ]
 }
 
 export default MapPage
