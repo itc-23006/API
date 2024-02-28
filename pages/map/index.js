1cb159d7baffca86a0dc60d69d94e8c859df043d
itc-23006 <n23006@std.it-college.ac.jp>
2024-02-28 10:47:47 +0000

    google_map_index
    reloadするたびに違う都道府県が標示される

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,8 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 
 const MapPage = () => {
   const [query, setQuery] = useState('Tokyo+Station')
+
+  useEffect(() => {
+    // ランダムな都道府県を生成する関数
+    const getRandomPrefecture = () => {
+      const prefectures = [
+        'Tokyo',
+        'Osaka',
+        'Kyoto',
+        'Hokkaido',
+        'Fukuoka',
+        'Aichi',
+        'Kanagawa'
+      ]
+      const randomIndex = Math.floor(Math.random() * prefectures.length)
+      return prefectures[randomIndex]
+    }
+
+    // ページが読み込まれた際にランダムな都道府県を設定
+    setQuery(getRandomPrefecture())
+  }, []) // 空の依存リストを渡すことで、コンポーネントが初回のみ実行されるようにする
+
   return (
     <div>
       <Head>
@@ -11,7 +32,6 @@ const MapPage = () => {
       </Head>
 
       <main>
-        {/* <iframe> コードを直接埋め込む */}
         <iframe
           width='600'
           height='450'
